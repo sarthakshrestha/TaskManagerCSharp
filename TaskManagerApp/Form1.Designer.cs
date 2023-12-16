0a0ce3b0d99393db48ad38efc736df9e09ccd46e
sarthaktbc <ssarthak20@tbc.edu.np>
2023-12-17 00:39:27 +0545

    Made Syntax Button to do the apps main processing

---
+++ TaskManagerApp/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TaskManagerApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.highRadioButton = new System.Windows.Forms.RadioButton();
+            this.mediumRadioButton = new System.Windows.Forms.RadioButton();
+            this.lowRadioButton = new System.Windows.Forms.RadioButton();
+            this.taskRemainingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SimpleTasker";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 146);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 57);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "What may be your task today?";
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(19, 223);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 3;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(110, 223);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(299, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Task Priorty";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // highRadioButton
+            // 
+            this.highRadioButton.AutoSize = true;
+            this.highRadioButton.Location = new System.Drawing.Point(303, 146);
+            this.highRadioButton.Name = "highRadioButton";
+            this.highRadioButton.Size = new System.Drawing.Size(56, 20);
+            this.highRadioButton.TabIndex = 6;
+            this.highRadioButton.TabStop = true;
+            this.highRadioButton.Text = "High";
+            this.highRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mediumRadioButton
+            // 
+            this.mediumRadioButton.AutoSize = true;
+            this.mediumRadioButton.Location = new System.Drawing.Point(303, 172);
+            this.mediumRadioButton.Name = "mediumRadioButton";
+            this.mediumRadioButton.Size = new System.Drawing.Size(76, 20);
+            this.mediumRadioButton.TabIndex = 7;
+            this.mediumRadioButton.TabStop = true;
+            this.mediumRadioButton.Text = "Medium";
+            this.mediumRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // lowRadioButton
+            // 
+            this.lowRadioButton.AutoSize = true;
+            this.lowRadioButton.Location = new System.Drawing.Point(303, 198);
+            this.lowRadioButton.Name = "lowRadioButton";
+            this.lowRadioButton.Size = new System.Drawing.Size(52, 20);
+            this.lowRadioButton.TabIndex = 8;
+            this.lowRadioButton.TabStop = true;
+            this.lowRadioButton.Text = "Low";
+            this.lowRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // taskRemainingLabel
+            // 
+            this.taskRemainingLabel.AutoSize = true;
+            this.taskRemainingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taskRemainingLabel.Location = new System.Drawing.Point(469, 101);
+            this.taskRemainingLabel.Name = "taskRemainingLabel";
+            this.taskRemainingLabel.Size = new System.Drawing.Size(186, 25);
+            this.taskRemainingLabel.TabIndex = 9;
+            this.taskRemainingLabel.Text = "Tasks for the day!";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.taskRemainingLabel);
+            this.Controls.Add(this.lowRadioButton);
+            this.Controls.Add(this.mediumRadioButton);
+            this.Controls.Add(this.highRadioButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "Task Manager App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton highRadioButton;
+        private System.Windows.Forms.RadioButton mediumRadioButton;
+        private System.Windows.Forms.RadioButton lowRadioButton;
+        private System.Windows.Forms.Label taskRemainingLabel;
+        private System.Windows.Forms.Panel taskPanel;
+    }
+}
+
---
+++ TaskManagerApp/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManagerApp
+{
+    public partial class MainWindow : Form
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            taskPanel = new Panel
+            {
+                AutoSize = true,
+                Location = new Point(303 + 40, 120),
+            };
+
+            // Add the panel to the form's controls
+            this.Controls.Add(taskPanel);
+        }
+
+        // Class that sets the text and priority
+        public class TaskItem
+        {
+            public string Task { get; set; }
+            public string Priority { get; set; }
+        }
+
+        // Storing the tasks in a list
+        private List<TaskItem> tasks = new List<TaskItem>();
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            // Retrieve text from text box
+            string taskRequired = textBox1.Text.Trim();
+
+            // Check if the user has entered a task
+            if (!string.IsNullOrEmpty(taskRequired))
+            {
+                // Retrieving the priority of the task
+                string priority = GetSelectedPriority();
+
+                // Creating a new checkbox for the task
+                CheckBox checkBox = CreateCheckBox(taskRequired);
+
+                // Adding the checkbox to the panel
+                taskPanel.Controls.Add(checkBox);
+
+                // Clear the text box for the next task
+                textBox1.Clear();
+
+                // Adding the task according to the priority
+                tasks.Add(new TaskItem { Task = taskRequired, Priority = priority });
+
+                // Displaying the priority as a popup
+                ShowPriorityPopup(taskRequired, priority);
+
+                // Show tasks in the task panel based on priority
+                ShowTaskPriority();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task");
+            }
+        }
+
+
+        private string GetSelectedPriority()
+        {
+            // Determine the selected priority from radio buttons
+            if (highRadioButton.Checked)
+            {
+                return "High";
+            }
+            else if (mediumRadioButton.Checked)
+            {
+                return "Medium";
+            }
+            else if (lowRadioButton.Checked)
+            {
+                return "Low";
+            }
+            else
+            {
+                // Default to Medium if no radio button is selected
+                return "Medium";
+            }
+        }
+
+        private void ShowTaskPriority()
+        {
+            // Clear existing controls in the task panel
+            taskPanel.Controls.Clear();
+
+            // Sort tasks first by priority, then by the order in which they were added
+            var sortedTasks = tasks.OrderBy(t => t.Priority).ThenBy(t => tasks.IndexOf(t)).ToList();
+
+            // Iterate through sorted tasks and display them
+            int yLocation = 0;  // Start from the top of the panel
+            foreach (var taskItem in sortedTasks)
+            {
+                CheckBox checkBox = CreateCheckBox(taskItem.Task);
+                checkBox.Location = new Point(0, yLocation);
+                yLocation += checkBox.Height + 10; // Adjust the vertical spacing
+                taskPanel.Controls.Add(checkBox);
+            }
+        }
+
+        private CheckBox CreateCheckBox(string taskRequired)
+        {
+            // Create a new CheckBox for the newly assigned task
+            CheckBox checkBox = new CheckBox
+            {
+                // The text will be updated to the newly assigned task
+                Text = taskRequired,
+
+                // The CheckBox will be automatically sized to fit the text
+                AutoSize = true,
+            };
+
+            // Return the CheckBox
+            return checkBox;
+        }
+
+        private void ShowPriorityPopup(string taskRequired, string priority)
+        {
+            // Display the priority as a popup
+            MessageBox.Show($"Task: {taskRequired}\nPriority: {priority}");
+        }
+    }
+}
